4626bdc182c83e513401981f2006c947a1efced2
tarek <tareeek.th@gmail.com>
2023-02-10 16:54:10 +0300

    fix buggs

--- module/rental.js
+++ module/rental.js
@@ -12,15 +12,15 @@ const Rental = mongoose.model("Rental", new mongoose.Schema({
                 minlength: 5,
                 maxlength: 50
             },
-            isGold: {
-                type: Boolean,
-                default: false
-            },
             phone: {
                 type: String,
                 required: true,
-                minlength: 10,
-                maxlength: 10
+                minlength: 5,
+                maxlength: 50
+            },
+            isGold: {
+                type: Boolean,
+                default: false
             }
         }),
         required: true
@@ -31,7 +31,7 @@ const Rental = mongoose.model("Rental", new mongoose.Schema({
                 type: String,
                 required: true,
                 trim: true,
-                minlength: 5,
+                minlength: 3,
                 maxlength: 50
             },
             dailyRentalRate: {
